cd5d7e2d2252894aa8a232289f4acebf6e7bfad0
TomLuna <lomtuna15@gmail.com>
2022-11-30 18:40:05 -0600

    first commit after creating react app

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,18 @@
+
+import About from './components/About';
+import Nav from './components/Nav';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Nav>
+      </Nav>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
