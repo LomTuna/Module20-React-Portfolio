881c5421ac6963da21ec015e06391c4b4fa54c80
TomLuna <lomtuna15@gmail.com>
2022-12-05 14:13:10 -0600

    updated the Contact form to populate as a SPA once clicked

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,19 +1,35 @@
-
+import React, {useState} from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
 import './App.css';
 import Projects from './components/Projects';
 import ContactForm from './components/Contact';
 
+
+
 function App() {
+  // const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
-      <Nav>
-      </Nav>
+      <Nav 
+        // categories={categories}
+        // setCurrentCategory={setCurrentCategory}
+        // currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+        ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Projects></Projects>
-        <About></About>
+        {!contactSelected ? (
+          <>
+          <Projects></Projects>
+          <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )
+        }
       </main>
     </div>
   );
--- react-portfolio/src/components/Nav/index.js
+++ react-portfolio/src/components/Nav/index.js
@@ -1,12 +1,69 @@
+// import React from 'react';
+// import { capitalizeFirstLetter } from '../../utils/helpers';
+
+// function Nav(props) {
+//   const {
+//     categories = [],
+//     setCurrentCategory,
+//     contactSelected,
+//     currentCategory,
+//     setContactSelected,
+//   } = props;
+
+//   return (
+//     <header className="flex-row px-1">
+//       <h2>
+//         <a data-testid="link" href="/">
+//           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+//         </a>
+//       </h2>
+//       <nav>
+//         <ul className="flex-row">
+//           <li className="mx-2">
+//             <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+//               About me
+//             </a>
+//           </li>
+//           <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+//             <span onClick={() => setContactSelected(true)}>Contact</span>
+//           </li>
+//           {categories.map((category) => (
+//             <li
+//               className={`mx-1 ${
+//                 currentCategory.name === category.name && !contactSelected && 'navActive'
+//                 }`}
+//               key={category.name}
+//             >
+//               <span
+//                 onClick={() => {
+//                   setCurrentCategory(category);
+//                   setContactSelected(false);
+//                 }}
+//               >
+//                 {capitalizeFirstLetter(category.name)}
+//               </span>
+//             </li>
+//           ))}
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// }
+
+// export default Nav;
 import React, { } from "react";
 
 
-function Nav() {
+function Nav(props) {
   
 
-  const handleClick = () => {
-    console.log("click handled")
-  }
+  // const handleClick = () => {
+  //   console.log("click handled")
+  // }
+  const {
+    contactSelected,
+    setContactSelected
+  } = props;
 
   return (
     <header data-testid="header" className="flex-row px-1">
@@ -18,28 +75,30 @@ function Nav() {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about" onClick={() => handleClick()}>
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
-          <li className={'mx-2'} >
-          <span onClick={() => handleClick()}>
-              Portfolio
-            </span>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
-          <li className={"mx-2"}>
-            <span onClick={() => handleClick()}>
-              Contact
-            </span>
-          </li>
-          <li className={"mx-2"}>
-            <span onClick={() => handleClick()}>
-              Resume
-            </span>
-          </li>
-          {
-           
-          }
+          {/* {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && !contactSelected && 'navActive'
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                  setContactSelected(false);
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))} */}
         </ul>
       </nav>
     </header>
